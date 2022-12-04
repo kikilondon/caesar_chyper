1fb43dd2d2f8e9da02ed0a5701c4ed28b821c8a2
Kiki <108157135+kikilondon@users.noreply.github.com>
2022-12-04 19:21:07 +0000

    Update cypher.js

--- cypher.js
+++ cypher.js
@@ -1,8 +1,3 @@
-// Your cypher is going to use the 15th letter after the letter you want to use.
-// For example, ‘a’ will be ‘p’. Bear in mind that the letters are treated as
-// cyclical. That means that ‘p’ will be coded to ‘e’ (the 10th letter after ‘p’ is ‘z’,
-// so 5 letters after that will be ‘e’ because the cycle starts again).
-
 
 /************* PSEUCODE ****************
  * Ask to the user to type a secret message and store it in a variable called "message"
